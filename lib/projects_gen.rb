d0308cf15e357d6cd346fd955c67cb40f5a86600
Alexander Shvets <alexander.shvets@gmail.com>
2013-08-31 21:56:12 -0400

    new version

--- lib/projects_gen.rb
+++ lib/projects_gen.rb
@@ -7,18 +7,22 @@ class ProjectsGen
   def generate_data file_name
     r = FileUtils.mkdir_p File.dirname file_name
 
-    items = JSON.parse(open(REPOS_URL).read)
+    items = JSON.parse(open(REPOS_URL).read).sort_by { |attr| attr['created_at'] }
 
     projects = []
 
     items.each do |item|
-      projects << {:name => item['name'], :url => item['url'].gsub('api.github.com/repos/', 'github.com/'), :description => item['description']}
+      projects << {:url => item['url'].gsub('api.github.com/repos/', 'github.com/'),
+                   :name => item['name'],
+                   :created_at => item['created_at'][0..9],
+                   :description => item['description']}
     end
+
 
     File.open(file_name, "w") do |file|
       projects.each do |project|
         file.write <<-CONTENT
-"#{project[:url]}", "#{project[:name]}", "#{project[:description]}"
+"#{project[:url]}", "#{project[:name]}", "#{project[:created_at]}", "#{project[:description]}"
         CONTENT
       end
     end
@@ -26,9 +30,9 @@ class ProjectsGen
 
   def read_data file_name
     File.open(file_name, "r").readlines().inject([]) do |result, line|
-      url, name, description = line.split(',')
+      url, name, created_at, description = line.split(',')
 
-      result << [name.gsub('"', ''), url.gsub('"', ''), description.gsub('"', '')]
+      result << [url.gsub('"', ''), name.gsub('"', ''), created_at.gsub('"', ''), description.gsub('"', '')]
 
       result
     end
