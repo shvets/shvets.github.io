4066435ae4607890d9088a7645e88dd6ed120be5
ashvets <Sheila01>
2014-08-22 14:27:19 -0400

    update presentations

--- presentations/run.sh
+++ presentations/run.sh
@@ -1,10 +1,12 @@
-slideshow build 01/vagrant-and-chef.md -t s6blank
-slideshow build 02/better-code-quality-with-tests.md -t s6blank
-slideshow build 03/nodejs-for-enterprise.md -t s6blank
+http://wkhtmltopdf.org/
 
-wkhtmltopdf --outline --orientation Landscape vagrant-and-chef.pdf.html 01/vagrant-and-chef.pdf
+slideshow build 01/vagrant-and-chef.md
+slideshow build 02/better-code-quality-with-tests.md
+slideshow build 03/nodejs-for-enterprise.md
 
-wkhtmltopdf --outline --orientation Landscape better-code-quality-with-tests.html 02/better-code-quality-with-tests.pdf
+wkhtmltopdf --outline --orientation Landscape --lowquality vagrant-and-chef.pdf.html 01/vagrant-and-chef.pdf
 
-wkhtmltopdf --outline --orientation Landscape nodejs-for-enterprise.pdf.html 03/nodejs-for-enterprise.pdf
+wkhtmltopdf --outline --orientation Landscape --lowquality better-code-quality-with-tests.pdf.html 02/better-code-quality-with-tests.pdf
+
+wkhtmltopdf --outline --orientation Landscape --lowquality nodejs-for-enterprise.pdf.html 03/nodejs-for-enterprise.pdf
 
